def788655ed191b529d8061dc687e9933e132ccd
Bruno Rocha <rochacbruno@gmail.com>
2014-02-02 17:43:32 -0200

    Create __init__.py

---
+++ taz/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+    Taz
+    ~~~~~
+
+    A Flask Full Stack Distribution
+
+    :copyright: (c) 2014 by Bruno Rocha.
+    :license: MIT, see LICENSE for more details.
+"""
+
+__version__ = '0.0.1'
