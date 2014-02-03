21591917f05552303f077041ed491228bc182896
Bruno Rocha <rochacbruno@gmail.com>
2014-02-03 02:26:37 -0200

    base structure

--- taz/__init__.py
+++ taz/__init__.py
@@ -10,3 +10,12 @@
 """
 
 __version__ = '0.0.1'
+__status__ = "Development"
+__description__ = "A Flask full stack distribution"
+__author__ = "Bruno Rocha <rochacbruno@gmail.com>"
+__email__ = "taz@pythonhub.com"
+__copyright__ = "Copyright 2014, PythonHub.com"
+
+
+# imports that are unused but exposed as public interface
+from .app import Taz
